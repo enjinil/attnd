7a05309452a30118144fae30e8672b5b9a6c792b
aaziez <azzalfauzi@gmail.com>
2024-09-13 20:39:08 +0700

    init phoenix with graphql

---
+++ api/lib/attendance_api_web/graphql/schema/content_types.ex
@@ -0,0 +1,7 @@
+defmodule AttendanceApiWeb.Schema.ContentTypes do
+  use Absinthe.Schema.Notation
+
+  object :hello_world do
+    field :message, :string
+  end
+end
---
+++ api/lib/attendance_api_web/router.ex
@@ -0,0 +1,29 @@
+defmodule AttendanceApiWeb.Router do
+  use AttendanceApiWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", AttendanceApiWeb do
+    pipe_through :api
+    forward "/api", GraphQl.Router
+  end
+
+  # Enable LiveDashboard and Swoosh mailbox preview in development
+  if Application.compile_env(:attendance_api, :dev_routes) do
+    # If you want to use the LiveDashboard in production, you should put
+    # it behind authentication and allow only admins to access it.
+    # If your application does not have an admins-only section yet,
+    # you can use Plug.BasicAuth to set up some basic authentication
+    # as long as you are also using SSL (which you should anyway).
+    import Phoenix.LiveDashboard.Router
+
+    scope "/dev" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      live_dashboard "/dashboard", metrics: AttendanceApiWeb.Telemetry
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+end
