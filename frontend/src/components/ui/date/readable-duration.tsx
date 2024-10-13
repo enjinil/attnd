21ea37f12a542dcca3ed4a5c2cfdcfd0c4098a4a
aaziez <azzalfauzi@gmail.com>
2024-10-13 21:57:07 +0700

    add ReadableDuration component

---
+++ frontend/src/components/ui/date/readable-duration.tsx
@@ -0,0 +1,40 @@
+type ReadableDurationProps = {
+  from: Date | string | undefined | null;
+  to: Date | string | undefined | null;
+}
+
+const ReadableDuration = ({ from, to }: ReadableDurationProps) => {
+  if(!from || !to) return null
+
+  // Convert dates to milliseconds
+  const fromMs = new Date(from).getTime();
+  const toMs = new Date(to).getTime();
+
+  // Calculate the difference in milliseconds
+  const durationMs = toMs - fromMs;
+
+  // Convert milliseconds to hours and minutes
+  const hours = Math.floor(durationMs / (1000 * 60 * 60));
+  const minutes = Math.floor((durationMs % (1000 * 60 * 60)) / (1000 * 60));
+
+  if (durationMs < 60000) {
+    return <span>less than a minute</span>;
+  }
+
+  if (hours === 0) {
+    return (
+      <span>
+        {minutes} minute{minutes !== 1 ? "s" : ""}
+      </span>
+    );
+  }
+
+  return (
+    <span>
+      {hours} hour{hours !== 1 ? "s" : ""} {minutes} minute
+      {minutes !== 1 ? "s" : ""}
+    </span>
+  );
+};
+
+export { ReadableDuration };
